ef50b58dfa42647b994ec450a15cdf9f4572f93e
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-06-21 21:25:07 -0500

    new endpoints

--- dist/routes/router.js
+++ dist/routes/router.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+var server_1 = __importDefault(require("../classes/server"));
+var socket_1 = require("../sockets/socket");
 exports.router = express_1.Router();
 // ===================================
 //GET MENSAJES
@@ -18,6 +23,12 @@ exports.router.get("/mensajes", function (req, res) {
 exports.router.post("/mensajes", function (req, res) {
     var cuerpo = req.body.cuerpo;
     var de = req.body.de;
+    var payload = {
+        cuerpo: cuerpo,
+        de: de,
+    };
+    var server = server_1.default.instance;
+    server.io.emit("mensaje-nuevo", payload);
     res.json({
         ok: true,
         cuerpo: cuerpo,
@@ -31,10 +42,40 @@ exports.router.post("/mensajes/:id", function (req, res) {
     var id = req.params.id;
     var cuerpo = req.body.cuerpo;
     var de = req.body.de;
+    var payload = {
+        de: de,
+        cuerpo: cuerpo,
+    };
+    var server = server_1.default.instance;
+    server.io.in(id).emit("mensaje-privado", payload);
     res.json({
         ok: true,
         id: id,
         cuerpo: cuerpo,
         de: de,
+    });
+});
+//Servicio para obtener el id de todos los usuarios
+exports.router.get("/usuarios", function (req, res) {
+    var server = server_1.default.instance;
+    server.io.clients(function (err, clientes) {
+        if (err) {
+            return res.json({
+                ok: false,
+                err: err,
+            });
+        }
+        res.json({
+            ok: true,
+            clientes: clientes,
+        });
+    });
+});
+//Obtener usuarios y sus nombres
+exports.router.get("/usuarios/detalle", function (req, res) {
+    var clientes = socket_1.usuariosConectados.getLista();
+    res.json({
+        ok: true,
+        clientes: clientes,
     });
 });
--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,7 @@
 import { Router, Request, Response } from "express";
+import Server from "../classes/server";
+import { Socket } from "socket.io";
+import { usuariosConectados } from "../sockets/socket";
 
 export const router = Router();
 
@@ -19,6 +22,14 @@ router.post("/mensajes", (req: Request, res: Response) => {
   const cuerpo = req.body.cuerpo;
   const de = req.body.de;
 
+  const payload = {
+    cuerpo,
+    de,
+  };
+
+  const server = Server.instance;
+  server.io.emit("mensaje-nuevo", payload);
+
   res.json({
     ok: true,
     cuerpo,
@@ -31,14 +42,48 @@ router.post("/mensajes", (req: Request, res: Response) => {
 // ===================================
 router.post("/mensajes/:id", (req: Request, res: Response) => {
   const id = req.params.id;
-
   const cuerpo = req.body.cuerpo;
   const de = req.body.de;
+
+  const payload = {
+    de,
+    cuerpo,
+  };
+
+  const server = Server.instance;
+  server.io.in(id).emit("mensaje-privado", payload);
 
   res.json({
     ok: true,
     id,
     cuerpo,
     de,
+  });
+});
+
+//Servicio para obtener el id de todos los usuarios
+router.get("/usuarios", (req: Request, res: Response) => {
+  const server = Server.instance;
+
+  server.io.clients((err: any, clientes: string[]) => {
+    if (err) {
+      return res.json({
+        ok: false,
+        err,
+      });
+    }
+    res.json({
+      ok: true,
+      clientes,
+    });
+  });
+});
+
+//Obtener usuarios y sus nombres
+router.get("/usuarios/detalle", (req: Request, res: Response) => {
+  const clientes = usuariosConectados.getLista();
+  res.json({
+    ok: true,
+    clientes,
   });
 });
