8f58d92f32d5bc4a81a8e21298807c83bf3e5afc
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-06-27 21:21:43 -0500

    repairing cors

--- index.ts
+++ index.ts
@@ -13,19 +13,19 @@ server.app.use(
 );
 server.app.use(bodyParser.json());
 
-//CORS
-// server.app.use(cors({ origin: true, credentials: true }));
+// CORS
+server.app.use(cors({ origin: true, credentials: true }));
 // Configurar cabeceras y cors
-server.app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 //RUTAS
 server.app.use("/", router);
